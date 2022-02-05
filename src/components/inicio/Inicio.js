b7be534a669ad4f56420f5b33ef050c7b263a73d
jjsus96 <jesus.sayago.mey.1996@gmail.com>
2022-02-05 04:47:49 +0100

    Menú y presentación listos

---
+++ src/components/inicio/Inicio.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Inicio.css';
+import video from './../../media/video.mp4'
+import Jesus from './../../media/Jesus.jpg'
+
+
+export const Inicio = () => {
+  return <div className='contenedor'>
+      <video className='video' src={video} autoPlay loop muted />
+      <h1 className='nombre'>Jesús Sayago Mey</h1>
+      <img src={Jesus} alt='' class="mx-auto d-block"/>
+      <h5 className='titulo'>| Desarrollador Web |</h5>
+      <p className='texto'>Buenas!, me llamo Jesús y soy Desarrollador Web. Aunque llevo poco tiempo en esto de la programación, mi camino en la informática se inició hace bastante, cuando algo en mi cabeza hizo click y decidí empezar a adentrarme en este mundo realizando los estudios de sistemas microinformáticos y redes. Una vez terminados decidí pasár al siguiente nivel y comencé a adentrarme en el mundo de la programación comenzando los estudios de Desarrollo de Aplicaciones Web y descubriendo un nuevo horizonte que aunque a veces causa muchos dolores de cabeza (Como ya sabréis), también aporta muchisima satisfacción y una increible sensación de crecimiento personal.</p>
+      <p className='texto'> </p>
+  </div>;
+};
+
+export default Inicio;
---
+++ src/components/navbar/Navbarcomp.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+
+export default class Navbarcomp extends Component {
+  render() {
+    return <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">Jesús Sayago Mey</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#formation">Formación</Nav.Link>
+            <Nav.Link href="#formation">Contacto</Nav.Link>
+          </Nav>
+          <Nav>
+          <NavDropdown title="Recuperaciones" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#tema1">Tema 1</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#tema3">Tema 3</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+</Navbar>
+    </div>;
+  }
+}
